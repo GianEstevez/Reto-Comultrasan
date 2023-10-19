7eb7c859b0aa7214dd31f029a4589351fe74ef0c
yurmel17 <yurmel1701@gmail.com>
2023-10-18 21:41:27 -0500

    feat: added fingerprint data

--- front-comprador/src/app/pago/pago.page.ts
+++ front-comprador/src/app/pago/pago.page.ts
@@ -10,10 +10,52 @@ export class PagoPage implements OnInit {
   mockCampos = mockCampos;
   negocioData: any = mockCampos.plantilla;
   compradorData: any = mockCampos.comprador;
+  fingerprintData: string = '';
 
   constructor(private router: Router) { }
 
   ngOnInit() {
+    const fechaHora = new Date();
+    console.log('Fecha y hora del llenado del formulario:', fechaHora);
+
+    const tipoDispositivo = navigator.userAgent;
+    console.log('Tipo de dispositivo:', tipoDispositivo);
+
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition( (position) => {
+        const latitud = position.coords.latitude;
+        const longitud = position.coords.longitude;
+        console.log('Ubicación actual (latitud, longitud):', latitud, longitud);
+
+        // Enviar datos al backend
+        this.enviarDatosAlBackend(fechaHora, tipoDispositivo, latitud, longitud);
+      });
+    } else {
+      console.log('El navegador no admite la geolocalización.');
+    }
+
+  }
+
+  enviarDatosAlBackend(fechaHora: any, tipoDispositivo: any, latitud: any, longitud: any) {
+    // Definir la URL del backend
+    const url = 'https://tu-servidor-backend.com/tu-endpoint';
+
+    // Datos a enviar al backend
+    const fingerprint = { fechaHora,
+      tipoDispositivo,
+      latitud,
+      longitud,
+    };
+
+    // Realiza una solicitud POST al servidor con los datos
+    //this.http.post(url, fingerprint).subscribe(
+    //  (respuesta) => {
+    //    console.log('Datos enviados al backend:', respuesta);
+    //  },
+    //  (error) => {
+    //    console.error('Error al enviar datos al backend:', error);
+    //  }
+    //);
   }
 
   onSubmit() {
